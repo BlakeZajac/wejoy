12e755fce6e79fe0fd0fef6056d31cde2fda303d
Blake Zajac <contact@blakezajac.com>
2023-07-30 16:15:50 +1000

    Fix Grid CSS

--- inc/enqueue/enqueue_css.php
+++ inc/enqueue/enqueue_css.php
@@ -6,5 +6,6 @@ if (!defined('ABSPATH')) {
 function my_theme_enqueue_styles()
 {
     wp_enqueue_style('custom-font-style', get_stylesheet_directory_uri() . '/css/font.css');
+    wp_enqueue_style('custom-grid-style', get_stylesheet_directory_uri() . '/css/grid.css');
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles', 99);
